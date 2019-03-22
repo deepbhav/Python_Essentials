2647c7ee3a124c0e457c46161bddbd56c0fba882
Deepak Bhavale <41051527+deepbhav@users.noreply.github.com>
2019-03-22 14:00:50 +0530

    Added Output to the 5th Question

--- Assignment5/Ass_5_5.py
+++ Assignment5/Ass_5_5.py
@@ -7,3 +7,14 @@ print("Total chars are {}".format(total))
 
 print("The Average of all Values is: {}".format(int(sum/total)))
 
+# Output
+# PS E:\Assignments\Python\Python_Essentials\Assignment5> python .\Ass_5_5.py
+# Enter the String : ABCDE
+# Char A = ASCII 65.
+# Char B = ASCII 66.
+# Char C = ASCII 67.
+# Char D = ASCII 68.
+# Char E = ASCII 69.
+# Sum is 335
+# Total chars are 5
+# The Average of all Values is: 67
